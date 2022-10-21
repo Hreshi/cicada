3956d330a0c0bf4a8e20104be17d7da00021a35d
hreshi <hkale746@gmail.com>
2022-10-21 22:21:34 +0530

    Init

---
+++ src/main/java/org/aissms/cicada/CicadaApplication.java
@@ -0,0 +1,12 @@
+package org.aissms.cicada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicadaApplication.class, args);
+	}
+}
---
+++ src/test/java/org/aissms/cicada/CicadaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aissms.cicada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
