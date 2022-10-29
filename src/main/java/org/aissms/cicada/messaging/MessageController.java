5f2346114507adcc9449b8e85c701e2e4429a853
hreshi <hkale746@gmail.com>
2022-10-29 11:12:53 +0530

    Message sending and recving working with oauth

--- src/main/java/org/aissms/cicada/messaging/MessageController.java
+++ src/main/java/org/aissms/cicada/messaging/MessageController.java
@@ -1,10 +1,13 @@
 package org.aissms.cicada.messaging;
 
+import java.security.Principal;
+
 import org.aissms.cicada.entity.ClientMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,8 +16,12 @@ public class MessageController {
     @Autowired public SimpMessagingTemplate template;
 
     @MessageMapping("/send")
-    public void sendMessage(Message<ClientMessage> message) {
+    public void sendMessage(Message<ClientMessage> message, OAuth2AuthenticationToken user) {
         ClientMessage ms = message.getPayload();
         template.convertAndSend("/messages/" + ms.getRecver(), ms);
+
+        System.out.println(ms.getContent());
+        System.out.println((String)user.getPrincipal().getAttribute("login"));
+        
     }
 }
