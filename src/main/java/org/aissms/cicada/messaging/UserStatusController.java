ae081dd5cf82d4a775a4e17dcc31814ea5e3af3d
hreshi <hkale746@gmail.com>
2022-10-30 13:11:14 +0530

    User status controller updated

--- src/main/java/org/aissms/cicada/messaging/UserStatusController.java
+++ src/main/java/org/aissms/cicada/messaging/UserStatusController.java
@@ -1,10 +1,8 @@
 package org.aissms.cicada.messaging;
 
-import org.aissms.cicada.entity.UserStatus;
 import org.aissms.cicada.entity.UserStatusMessage;
 import org.aissms.cicada.service.UserStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
