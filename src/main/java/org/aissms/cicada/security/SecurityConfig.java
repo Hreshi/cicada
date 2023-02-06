ff55071b4fadd73c40d07372b4f4a21b1e61cf43
hreshi <hkale746@gmail.com>
2023-02-06 23:07:55 +0530

    Added filter for jwt

--- src/main/java/org/aissms/cicada/security/SecurityConfig.java
+++ src/main/java/org/aissms/cicada/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests(req->{
 			req
-			.antMatchers("/register", "/login", "/logout")
+			.antMatchers("/register", "/login", "/page", "/js/*","/css/*")
 			.permitAll()
 			.anyRequest()
 			.authenticated();
