a593fca3088d9d013149e57d7df8cacd51b11817
hreshi <hkale746@gmail.com>
2023-03-21 14:03:06 +0530

    Add RequestBody annotation to inject request body

--- src/main/java/org/aissms/cicada/auth/LoginController.java
+++ src/main/java/org/aissms/cicada/auth/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,7 +20,7 @@ public class LoginController {
     @Autowired JwtTokenUtil jwtTokenUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(LoginForm form) {
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginForm form) {
         User user = userService.findByEmail(form.getEmail());
         if(user == null) System.out.println("user is null");
         if(user == null || !encoder.matches(form.getPassword(), user.getPassword())) {
