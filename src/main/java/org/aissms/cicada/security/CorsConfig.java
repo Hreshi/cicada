a2fb01184add5db7b599ea870770d9b1594e1a42
hreshi <hkale746@gmail.com>
2023-03-22 10:53:21 +0530

    Cors config

--- src/main/java/org/aissms/cicada/security/CorsConfig.java
+++ src/main/java/org/aissms/cicada/security/CorsConfig.java
@@ -11,7 +11,11 @@ public class CorsConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+        .addMapping("/*")
+        .allowedHeaders("*")
+        .allowedMethods("*")
+        .allowedOrigins("*");
     }
     
 }
