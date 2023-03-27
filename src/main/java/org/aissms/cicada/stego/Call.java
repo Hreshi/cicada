b3bdfcef92739d057203f3a0e8f194e522239f22
hreshi <hkale746@gmail.com>
2023-03-27 22:36:46 +0530

    synchronize two users on call

--- src/main/java/org/aissms/cicada/stego/Call.java
+++ src/main/java/org/aissms/cicada/stego/Call.java
@@ -1,16 +1,24 @@
 package org.aissms.cicada.stego;
 
+import java.util.ArrayList;
+
 class Call {
     // email1 is the caller
     String email1;
     String email2;
     boolean callStarted;
+    boolean store;
+    ArrayList<String> msgFromEmail1;
+    ArrayList<String> msgFromEmail2;
     long ringEndTime;
 
     public Call(String user1, String user2, long ringEndTime) {
         this.email1 = user1;
         this.email2 = user2;
         this.ringEndTime = ringEndTime;
+        this.store = true;
+        msgFromEmail1 = new ArrayList<>();
+        msgFromEmail2 = new ArrayList<>();
     }
 
     public String getEmail1() {
@@ -28,11 +36,22 @@ class Call {
     public void setCallStarted(boolean callStarted) {
         this.callStarted = callStarted;
     }
-
+    public void stopStoring() {
+        store = false;
+    }
+    public boolean canStore() {
+        return store;
+    }
     public long getRingEndTime() {
         return ringEndTime;
     }
     public boolean ringExpired() {
         return System.currentTimeMillis() > ringEndTime;
+    }
+    public ArrayList<String> getMsgFromEmail1() {
+        return msgFromEmail1;
+    }
+    public ArrayList<String> getMsgFromEmail2() {
+        return msgFromEmail2;
     }
 }
--- src/main/java/org/aissms/cicada/stego/CallMessageController.java
+++ src/main/java/org/aissms/cicada/stego/CallMessageController.java
@@ -1,6 +1,7 @@
 package org.aissms.cicada.stego;
 
 import java.util.Date;
+import java.util.List;
 
 import org.aissms.cicada.messaging.MyMessageDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,16 +24,45 @@ public class CallMessageController {
         Call call = callRepository.getCall(auth.getName());
         if(call == null) return;
 
-        String secondUser = call.getEmail1();
-        if(secondUser.equals(auth.getName())) {
-            secondUser = call.getEmail2();
+        String email1 = call.getEmail1();
+        String email2 = call.getEmail2();
+
+        if(call.canStore()) {
+            List<String> list1 = call.getMsgFromEmail1();
+            List<String> list2 = call.getMsgFromEmail2();
+
+            if(email1.equals(auth.getName())) {
+                list1.add(message);
+            } else {
+                list2.add(message);
+            }
+            if(list1.size() > 0 && list2.size() > 0) {
+                call.stopStoring();
+                for(String msg : list1) {
+                    messagingTemplate.convertAndSend("/messages/"+email2, toDto(email1, msg));
+                }
+                for(String msg : list2) {
+                    messagingTemplate.convertAndSend("/messages/"+email1, toDto(email1, msg));
+                }
+                list1.clear();
+                list2.clear();
+            }
+        } else {
+            System.out.println("\n\nMessage : " + message + "\n\n");
+            MyMessageDto dto = toDto(auth.getName(), message);
+            if(email1.equals(auth.getName())) {
+                messagingTemplate.convertAndSend("/messages/"+email2, dto);
+            } else {
+                messagingTemplate.convertAndSend("/messages/"+email1, dto);
+            }
         }
+    }
+    private MyMessageDto toDto(String email, String msg) {
         MyMessageDto dto = new MyMessageDto();
-        dto.setAuthor(auth.getName());
-        dto.setBlockIndex(0);
-        dto.setContent(message);
-        dto.setMessagetype("SECRET");
+        dto.setAuthor(email);
+        dto.setContent(msg);
         dto.setDate(new Date());
-        messagingTemplate.convertAndSend("/messages/"+secondUser, dto);
+        dto.setMessagetype("SECRET");
+        return dto;
     }
 }
